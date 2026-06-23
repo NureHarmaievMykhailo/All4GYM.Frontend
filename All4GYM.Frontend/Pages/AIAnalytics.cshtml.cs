88ee2161a2eb76e43a177bb1f9f983ce48d86912
Михайло Гармаєв <mykhailo.harmaiev@nure.ua>
2026-06-23 06:28:06 +0300

    Add .gitignore and update AI analytics

--- All4GYM.Frontend/Pages/AIAnalytics.cshtml.cs
+++ All4GYM.Frontend/Pages/AIAnalytics.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace All4GYM.Frontend.Pages;
 
+[IgnoreAntiforgeryToken]
 public class AIAnalyticsModel : BasePageModel
 {
     private readonly IHttpClientFactory _httpClientFactory;
